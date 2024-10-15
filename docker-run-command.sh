62a3433518079564df360a6e8f0318d0e23b47d2
hrtsv <hartsvc@gmail.com>
2024-10-15 01:23:26 -0700

    Update docker-run-command.sh

--- docker-run-command.sh
+++ docker-run-command.sh
@@ -3,11 +3,12 @@ docker run -d \
   --runtime=nvidia \
   -e NVIDIA_VISIBLE_DEVICES=all \
   -p 5000:5000 \
-  -v /path/to/data:/app/data \
+  -v /opt/stacks/ipmi-gpu-monitor/data:/app/data \
   -e SECRET_KEY=your_secret_key_here \
   -e JWT_SECRET_KEY=your_jwt_secret_key_here \
   -e IPMI_HOST=your_ipmi_host \
   -e IPMI_USERNAME=your_ipmi_username \
   -e IPMI_PASSWORD=your_ipmi_password \
   --device /dev/ipmi0:/dev/ipmi0 \
-  ghcr.io/yourusername/ipmi-gpu-monitor:latest
+  $(docker build -q https://github.com/hrtsv/ipmi-gpu-monitor.git)
+  
