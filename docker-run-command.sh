4228e79b07211fb72a9e348eccd07955216007ad
hrtsv <hartsvc@gmail.com>
2024-10-22 13:04:19 -0700

    Update docker-run-command.sh

--- docker-run-command.sh
+++ docker-run-command.sh
@@ -4,7 +4,6 @@ docker run -d \
   -e NVIDIA_VISIBLE_DEVICES=all \
   -p 5000:5000 \
   -v /opt/stacks/ipmi-gpu-monitor/data:/app/data \
-  -e SECRET_KEY=your_secret_key_here \
   -e IPMI_HOST=your_ipmi_host \
   -e IPMI_USERNAME=your_ipmi_username \
   -e IPMI_PASSWORD=your_ipmi_password \
